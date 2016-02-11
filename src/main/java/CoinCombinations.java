342850ade692c271b9e6875d1da060e624de4782
Cassandra Culkins and Ramon de la Cruz <cassy.culkins@gmail.com>
2016-02-11 09:59:12 -0800

    while loop working

--- src/main/java/CoinCombinations.java
+++ src/main/java/CoinCombinations.java
@@ -8,10 +8,25 @@ public class CoinCombinations {
   public static void main(String[] args) {
 
   }
-    public static Integer checkCoins(Integer change) {
-      if (change >= 25) {
-        return 25;
-      } else {
-    } return 0;
-  }
+    public static Integer giveChange(Integer change) {
+      Integer quarters = 0;
+      Integer dimes = 0;
+      Integer nickels = 0;
+      Integer pennies = 0;
+      while (change > 0) {
+        if (change >= 25) {
+          change -= 25;
+          quarters ++;
+        } else if (change >= 10) {
+         change -= 10;
+         dimes ++;
+        } else if (change >= 5) {
+         change -= 5;
+         nickels ++;
+        } else {
+         change -= 1;
+         pennies ++;
+        }
+      } return change;
+    }
 }
--- src/test/java/CoinCombinationsTest.java
+++ src/test/java/CoinCombinationsTest.java
@@ -3,15 +3,29 @@ import static org.junit.Assert.*;
 
 public class CoinCombinationsTest {
 
+  // @Test
+  // public void checkCoins_returnsOneQuarterWhenGivenValueOf25_1Quarter(){
+  //   CoinCombinations coinCombinationTest = new CoinCombinations();
+  //   assertEquals(25, (int) coinCombinationTest.checkCoins(25));
+  // }
+  //
+  // @Test
+  // public void checkCoins_returnsOneDimeWhenGivenValueof10_1Dime() {
+  //   CoinCombinations coinCombinationTest = new CoinCombinations();
+  //   assertEquals(10, (int) coinCombinationTest.checkCoins(10));
+  // }
+  //
+  // @Test
+  // public void checkCoins_returnOneNickelWhenGivenValueOf5_1Nickel() {
+  //   CoinCombinations coinCombinationTest = new CoinCombinations();
+  //   assertEquals(5, (int) coinCombinationTest.checkCoins(5));
+  // }
+
   @Test
-  public void checkCoins_returnsOneQuarterWhenGivenValueOf25_1Quarter(){
+  public void giveChange_returnsCorrectChangeWhenGivenAValueOf99_3Quarters2Dimes0Nickels4Pennies() {
     CoinCombinations coinCombinationTest = new CoinCombinations();
-    assertEquals(25, (int) coinCombinationTest.checkCoins(25));
-  }
-  @Test
-  public void checkCoins_returnsOneDimeWhenGivenValueof10_1Dime() {
-    CoinCombinations coinCombinationTest = new CoinCombinations();
-    assertEquals(10, (int) coinCombinationTest.checkCoins(10));
+    Integer change = 99;
+    assertEquals(0, (int) coinCombinationTest.giveChange (99));
   }
 
 }
